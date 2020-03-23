3cad703b8fdf7bb736be15b87a82979dd69b3834
jacob <59165596+jacob-srs@users.noreply.github.com>
2020-03-23 14:24:49 +0800

    一些补充

--- faceRecognition/bisect_source_image.py
+++ faceRecognition/bisect_source_image.py
@@ -23,18 +23,19 @@ class Source_2_Regist_Test():
     
     def __init__(self, parser_args=False, command_tuple=None):
         
+        self.source_path = ""  # 字符串声明
+        
+        # 对命令行模式或调试模式进行判断，对命令行参数进行判断
         if command_tuple is not None:  # 调试模式
             self.__sys_argv = command_tuple
             if parser_args is False:  # parser_arg == False，输入三个地址
                 self.__check_all_paths()
-                # self.__init_argparse()
             elif parser_args is True:  # 输入 source 地址
                 self.__check_source_path()            
         else:
             self.__sys_argv = self.__read_command_args_use_sys()
             if parser_args is False:  # parser_arg == False，输入三个地址
                 self.__check_all_paths()
-                # self.__init_argparse()
             elif parser_args is True:  # 输入 source 地址
                 self.__check_source_path()
 
@@ -48,22 +49,64 @@ class Source_2_Regist_Test():
         
         pass
 
-    def __check_source_path(self):
+    def __check_source_path(self):  # 目前主要用这个函数
         """ TODO 接收 source 路径并将 register 与 test 保存在于 source 所在目录中
         
-        由于时间有限，"--source_only" 的位置写死
+        由于时间有限，"--source_only" 的位置写死，
+        所以 argv[0] = 文件.py，argv[1] = --source_only， argv[2] = 路径
         """
         print("in check source path")
         
-        if len(self.__sys_argv) < 2:  # 没有输入 source 路径
+        source_path = ""  # 声明空字符串
+        if len(self.__sys_argv) < 3:  # 没有输入 source 路径
             print("\n..Error::source path is not entered, exiting...")
             sys.exit(0)
         
-        # source_not_exist_flag = False
-        # if len(self.__sys_argv) < 2 or not os.path.exists(self.__sys_argv[1]):
-        if not os.path.exists(self.__sys_argv[1]):
+        if not os.path.exists(self.__sys_argv[2]):
             print("\n..Error::source path is not  exist, exiting...")
             sys.exit(0)
+
+        source_path = self.__sys_argv[2]
+        self.source_path = source_path
+        return source_path
+
+    def __make_dir(self, dir_name):
+        """ 实例私有函数，判断并创建目录 """
+        
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
+        else:
+            pass
+
+    def create_dir_register_test(self):
+        """ TODO 创建 register, test 文件夹
+        
+        注1：尝试 os.getcwd()
+        注2：source，register，test都在同级目录下
+        """
+        
+        print("in create_dir_register_test")      
+
+        # 创建 source 同级分类目录 register 与 test
+        root_path = "\\".join(self.source_path.split("\\")[:-1])
+        source_name = self.source_path.split("\\")[-1]
+        register_dir_name = source_name + "_register"
+        test_dir_name = source_name + "_test"
+
+        register_dir_path = os.path.join(root_path, register_dir_name)
+        test_dir_path = os.path.join(root_path, test_dir_name)
+
+        self.__make_dir(register_dir_path)
+        self.__make_dir(test_dir_path)
+       
+        # 在分类目录中循环复制 source 中的文件结构， glob？
+        source_sub_dirs = os.listdir((self.source_path))
+        for name_sub_dir in source_sub_dirs:
+            register_sub_dir = os.path.join(register_dir_path, name_sub_dir)
+            test_sub_dir = os.path.join(test_dir_path, name_sub_dir)
+
+            self.__make_dir(register_sub_dir)
+            self.__make_dir(test_sub_dir)
     
     def __check_all_paths(self):
         """ 读取命令行输入的路径 """
@@ -94,7 +137,7 @@ class Source_2_Regist_Test():
         
         return self.__sys_argv
 
-    def create_directory(self):
+    def create_directory(self, dir_name):
         """ TODO 创建文件目录 """
         
         pass
@@ -126,3 +169,5 @@ if __name__ == "__main__":
     s2rt = Source_2_Regist_Test(parser_flag, command_tuple)
     argv = s2rt.get_command_args()
     print("\n参数列表::%s，参数数目::%s" %(str(argv), len(argv)))
+
+    s2rt.create_dir_register_test()
